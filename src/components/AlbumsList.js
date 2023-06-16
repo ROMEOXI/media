0a96c0f42515db24966dae08ec69e3a95c6cf7ae
WangXi <jason942371@gmail.com>
2023-06-16 15:56:35 +0800

    Build useAddAlbumMutation Use tag system approach

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,12 +1,42 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
-  const results = useFetchAlbumsQuery(user);
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
 
-  console.log(results);
-  // console.log(data, error, isLoading);
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+  
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albuns.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
 
-  return <div>Albums for {user.name}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos of the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>
+        Albums for {user.name}
+        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
