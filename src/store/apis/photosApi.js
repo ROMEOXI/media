23525c95c585b507b5074c22ebc6855ff39ea1a2
WangXi <jason942371@gmail.com>
2023-06-18 16:13:39 +0800

    Add removePhoto feature into PhotosListItem. Update photosApi with Tags.

--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { faker } from "@faker-js/faker";
+import { albumsApi } from "./albumsApi";
 
 const photosApi = createApi({
   reducerPath: "photos",
@@ -9,6 +10,13 @@ const photosApi = createApi({
   endpoints(builder) {
     return {
       fetchPhotos: builder.query({
+        providesTags: (result, error, album) => {
+          const tags = result.map((photo) => {
+            return { type: "Photo", id: photo.id };
+          });
+          tags.push({ type: "AlbumPhoto", id: album.id });
+          return tags;
+        },
         query: (album) => {
           return {
             url: "/photos",
@@ -20,6 +28,9 @@ const photosApi = createApi({
         },
       }),
       addPhoto: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "AlbumPhoto", id: album.id }];
+        },
         query: (album) => {
           return {
             method: "POST",
@@ -32,10 +43,13 @@ const photosApi = createApi({
         },
       }),
       removePhoto: builder.mutation({
+        invalidatesTags: (result, error, photo) => {
+          return [{ type: "Photo", id: photo.id }];
+        },
         query: (photo) => {
           return {
             method: "DELETE",
-            url: `/phtos/${photo.id}`,
+            url: `/photos/${photo.id}`,
           };
         },
       }),
